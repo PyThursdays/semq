1c3f25d3d56782d06bff7759e712ca65fcaa17d1
rhdzmota <contact@rhdzmota.com>
2023-02-20 17:48:14 -0600

    Add logger and new env.vars

--- src/semq/settings.py
+++ src/semq/settings.py
@@ -1,14 +1,20 @@
-import logging
 import os
+import logging
+from typing import Optional
 
 SEMQ_DEFAULT_HELLO_WORLD = os.environ.get(
     "SEMQ_DEFAULT_HELLO_WORLD",
     default="world",
 )
 
-SEMQ_DEFAULT_TRANSACTION_LOG_PATH = os.environ.get(
-    "SEMQ_DEFAULT_TRANSACTION_LOG_PATH",
-    default="."
+SEMQ_DEFAULT_METASTORE_PATH = os.environ.get(
+    "SEMQ_DEFAULT_METASTORE_PATH",
+    default=os.path.abspath(os.path.join(".", "metastore"))
+)
+
+SEMQ_DEFAULT_METASTORE_TRASHDIR = os.environ.get(
+    "SEMQ_DEFAULT_METASTORE_TRASHDIR",
+    default=".trash",
 )
 
 SEMQ_DEFAULT_PARTITION_SIZE = int(os.environ.get(
@@ -25,6 +31,14 @@ SEMQ_DEFAULT_PARTITION_FILE_ENDING = os.environ.get(
 if not SEMQ_DEFAULT_PARTITION_FILE_ENDING.startswith("."):
     SEMQ_DEFAULT_PARTITION_FILE_ENDING = "." + SEMQ_DEFAULT_PARTITION_FILE_ENDING
 
+SEMQ_DEFAULT_LOGGING_LEVEL = os.environ.get(
+    "SEMQ_DEFAULT_LOGGING_LEVEL",
+    default="INFO",
+)
 
-def get_logger(name: str):
-    return logging.getLogger(name)
+
+def get_logger(name: str, log_level: Optional[str] = None):
+    logging.basicConfig()
+    logger = logging.getLogger(name)
+    logger.setLevel(log_level or SEMQ_DEFAULT_LOGGING_LEVEL)
+    return logger
